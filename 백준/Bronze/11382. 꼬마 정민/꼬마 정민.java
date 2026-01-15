6f44be9073f77408f924b267ef818ae6a1f4c02a
gyujin <softworldqjin@gmail.com>
2026-01-15 18:18:57 +0900

    [Bronze V] Title: 꼬마 정민, Time: 172 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        long a = input.nextLong();
+        long b = input.nextLong();
+        long c = input.nextLong();
+        
+        System.out.println(a + b  +c);
+    }
+}
