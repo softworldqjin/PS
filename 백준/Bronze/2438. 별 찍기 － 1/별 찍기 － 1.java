380951cfe004b3e91c0550e5bcb21caa974e4f62
gyujin <softworldqjin@gmail.com>
2026-01-30 21:45:17 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 248 ms, Memory: 17980 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        
+        for (int i = 0; i < num; i++){
+            for (int j = 0; j < i + 1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
