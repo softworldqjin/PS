310b695cdfe73690fa9b1dc72154ba19b99cabee
gyujin <softworldqjin@gmail.com>
2026-02-05 23:05:05 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 168 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+public static void main(String[] args){
+ 
+    Scanner scanner = new Scanner(System.in);
+    String str = scanner.next();
+    int length = str.length();
+    
+    System.out.println(length);
+    
+}
+}
