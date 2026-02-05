ff993c19528c4057595f548cb431e56612431a86
gyujin <softworldqjin@gmail.com>
2026-02-05 23:05:39 +0900

    [Bronze V] Title: 아스키 코드, Time: 156 ms, Memory: 17552 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main{
+public static void main(String[] args){
+
+    Scanner scanner = new Scanner(System.in);
+    char c = scanner.next().charAt(0);
+    int result = c;
+    
+    System.out.println(result);
+}
+}
