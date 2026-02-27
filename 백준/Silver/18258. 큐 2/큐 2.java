669744b60e48e0edb0f62b6afa9ee69999d753f3
gyujin <softworldqjin@gmail.com>
2026-02-27 23:44:04 +0900

    [Silver IV] Title: 큐 2, Time: 1280 ms, Memory: 360040 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -2,50 +2,40 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         Deque<Integer> q = new ArrayDeque<>();
-        int n = Integer.parseInt(br.readLine());
         
+        int n = Integer.parseInt(br.readLine());
         for (int i = 0; i < n; i++){
             String s = br.readLine();
-            String option = s.split(" ")[0];
+            int x;
             
-            Integer x;
-            if ("push".equals(option)){
+            if (s.startsWith("push")){
                 x = Integer.parseInt(s.split(" ")[1]);
                 q.offer(x);
-            } else if ("pop".equals(option)){
-                x = q.poll();
-                if (x == null){
-                    x = -1;
-                }
+            } else if (s.startsWith("pop")){
+                x = q.isEmpty() ? -1 : q.poll();
                 sb.append(x).append('\n');
-            } else if ("size".equals(option)){
+                
+            } else if (s.startsWith("size")){
                 x = q.size();
                 sb.append(x).append('\n');
-            } else if ("empty".equals(option)){
-                if (q.isEmpty()){
-                    x = 1;
-                } else {
-                    x = 0;
-                }
+                
+            } else if (s.startsWith("empty")){
+                x = q.isEmpty() ? 1 : 0;
                 sb.append(x).append('\n');
-            } else if ("front".equals(option)){
-                x = q.peekFirst();
-                if (x == null){
-                    x = -1;
-                }
+                
+            } else if (s.startsWith("front")){
+                x = q.isEmpty() ? -1 : q.peekFirst();
                 sb.append(x).append('\n');
-            } else if ("back".equals(option)){
-                x = q.peekLast();
-                if (x == null){
-                    x = -1;
-                }
+                
+            } else if (s.startsWith("back")){
+                x = q.isEmpty() ? -1 : q.peekLast();
                 sb.append(x).append('\n');
-            }
+            } 
         }
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
