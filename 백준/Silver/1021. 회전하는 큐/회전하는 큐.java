7202e549c8929fd09c6351baa453738685a15e3b
gyujin <softworldqjin@gmail.com>
2026-03-08 00:56:28 +0900

    [Silver III] Title: 회전하는 큐, Time: 172 ms, Memory: 17768 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -2,34 +2,33 @@ import java.util.*;
 
 public class Main{
     public static void main(String[] args){
-        LinkedList<Integer> dq = new LinkedList<>();
         Scanner sc = new Scanner(System.in);
-        
+        int length = sc.nextInt();
         int n = sc.nextInt();
-        int m = sc.nextInt();
-        for (int i = 1; i <= n; i++){
-            dq.addLast(i);
+        LinkedList<Integer> q = new LinkedList<>();
+        
+        for (int i = 1; i < length + 1; i++){
+            q.addLast(i);
         }
         
         int count = 0;
-        for (int i = 0; i < m; i++){
+        for (int i = 0; i < n; i++){
             int t = sc.nextInt();
+            int front = q.indexOf(t);
+            int last = q.size() - front;
             
-            int front = dq.indexOf(t);
-            int last = dq.size() - front;
             if (front >= last){
                 for (int j = 0; j < last; j++){
-                    dq.addFirst(dq.pollLast());
+                    q.addFirst(q.pollLast());
                     count++;
                 }
             } else {
                 for (int j = 0; j < front; j++){
-                    dq.addLast(dq.pollFirst());
+                    q.addLast(q.pollFirst());
                     count++;
                 }
             }
-            
-            dq.pollFirst();
+            q.pollFirst();
         }
         System.out.println(count);
     }
