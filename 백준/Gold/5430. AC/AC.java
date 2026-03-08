6fa043f3256df9b267d8f0f0c0e8131f93b04bcd
gyujin <softworldqjin@gmail.com>
2026-03-08 23:38:54 +0900

    [Gold V] Title: AC, Time: 648 ms, Memory: 79156 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -4,60 +4,63 @@ import java.io.*;
 public class Main{
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         int n = Integer.parseInt(br.readLine());
         for (int i = 0; i < n; i++){
             String f = br.readLine();
-            char[] fArray = f.toCharArray();
-            int arrayCount = Integer.parseInt(br.readLine());
+            int length = Integer.parseInt(br.readLine());
+            String s = br.readLine();
             Deque<String> dq = new ArrayDeque<>();
             
-            String s = br.readLine();
-            if (arrayCount > 0){
-                String[] str = s.replace("[", "").replace("]", "").split(",");
-                for (int j = 0; j < arrayCount; j++){
-                    dq.offerLast(str[j]);
+            if (length > 0){
+                String[] array = s.replace("[", "").replace("]", "").split(",");
+                for (int j = 0; j < length; j++){
+                    dq.addLast(array[j]);
                 }
             }
             
             boolean result = false;
             boolean reverse = false;
-            for (char c : fArray){
+            for (char c : f.toCharArray()){
                 if (c == 'R'){
                     reverse = !reverse;
-                } else if (c == 'D'){
+                } else {
                     if (dq.isEmpty()){
                         result = true;
                         break;
                     }
-                    if (!reverse){
-                        dq.pollFirst();
-                    } else {
-                        dq.pollLast();
+                    else {
+                        if (reverse){
+                            dq.pollLast();
+                        } else {
+                            dq.pollFirst();
+                        }
                     }
                 }
             }
+            StringBuilder sb = new StringBuilder();
             if (result){
                 System.out.println("error");
-            } else{
-                StringBuilder sb = new StringBuilder();
+            } else {
                 sb.append("[");
-                
-                while (!dq.isEmpty()){
-                    if (!reverse){
-                        sb.append(dq.pollFirst());
-                    } else {
+                if (reverse){
+                    while (!dq.isEmpty()){
                         sb.append(dq.pollLast());
+                        if (!dq.isEmpty()){
+                            sb.append(",");
+                        }
                     }
-                    
-                    if (!dq.isEmpty()){
-                        sb.append(",");
+                } else {
+                    while (!dq.isEmpty()){
+                        sb.append(dq.pollFirst());
+                            if (!dq.isEmpty()){
+                                sb.append(",");
+                            }
+                        }
                     }
-                }
+                
                 sb.append("]");
                 System.out.println(sb);
             }
-            
         }
     }
 }
