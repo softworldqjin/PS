5b5b60f5aca067559a54a0b65d70a558261bc37d
gyujin <softworldqjin@gmail.com>
2026-05-09 23:37:35 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 2.48 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,44 +1,6 @@
-import java.util.*;
-
 class Solution {
-	    public int solution(int[][] jobs) {
-	        int answer = 0;
-	        int end = 0; 
-	        int index = 0;
-	        int count =0; 
-	        
-	        Arrays.sort(jobs, new Comparator<int[]>() {	       	
-				@Override
-				public int compare(int[] o1, int[] o2) {
-					return o1[0] - o2[0];
-				}      	
-			});
-	        
-	        PriorityQueue<int[]> q = new PriorityQueue<int[]>(new Comparator<int[]>() {
-
-				@Override
-				public int compare(int[] o1, int[] o2) {
-					return o1[1] - o2[1];
-				}
-			});
-
-	        while(count < jobs.length) {
-	        	
-	        	while(index < jobs.length && jobs[index][0] <= end) {
-	        		q.add(jobs[index++]);
-	        	}
-	        	
-	        	if(q.isEmpty()) {
-	        		end = jobs[index][0];
-	        	}else {
-	        		int[] temp = q.poll();
-	        		answer += temp[1] + end - temp[0];
-	        		end += temp[1];
-	        		count++;
-	        	}
-	        	
-	        }
-	        
-	        return answer/jobs.length;
-	    }
+    public int solution(int[][] jobs) {
+        int answer = 0;
+        return answer;
+    }
 }
